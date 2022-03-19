bda7272a0f5fd0dee25648a0001201d88a2d388f
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2022-03-19 17:03:57 +0100

    Add copyright + steam and email buttons

---
+++ assets/js/copyright.js
@@ -0,0 +1,5 @@
+const footer = document.querySelector('#copyright');
+
+footer.innerHTML = `Copyright &copy; ${new Date().getFullYear()} Lulu. All Rights Reserved`;
+
+console.log('%cHello there :)', 'font-size:2rem;font-weight:bold;color:red;');
