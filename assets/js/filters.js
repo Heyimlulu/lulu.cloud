6a7d100b9f9c3738e541f727710a0378ffa87a90
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2022-04-09 09:20:16 +0200

    Fixed preloaded and started working on portfolio

---
+++ assets/js/filters.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    // filter portfolio items by category when clicking on a filter button (li)
+    $('.filters li').click(function () {
+        $('.filters li').removeClass('active');
+        $(this).addClass('active');
+        var filter = $(this).attr('data-filter');
+        $('.portfolio-items .item').each(function () {
+            if ($(this).hasClass(filter.split('.')[1]) || filter == '*') {
+                $(this).fadeIn(200);
+            } else {
+                $(this).fadeOut(200);
+            }
+        });
+    });
+});
